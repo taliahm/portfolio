0fb52361d81bc9f11eaccad41e76a21db16ef179
Talia Harrison-Marcassa <hm.talia@gmail.com>
2017-03-26 17:59:54 -0400

    small fixes to styling

--- public/app.js
+++ public/app.js
@@ -5,8 +5,6 @@ console.log('hello');
 
 var portApp = {};
 
-portApp.scroll = function () {};
-
 portApp.hamburgerClick = function () {
 	$('#hamburger').on('click', function () {
 		// e.preventDefault()
@@ -32,13 +30,55 @@ portApp.clickPortfolio = function () {
 	});
 };
 
+portApp.burgerMenuScroll = function () {
+	$('#menuAbout').on('click', function (e) {
+		e.preventDefault();
+		var targetElem = $('#about');
+		portApp.scroll(targetElem);
+	});
+	$('#menuSkill').on('click', function (e) {
+		e.preventDefault();
+		var targetElem = $('#skills');
+		portApp.scroll(targetElem);
+	});
+	$('#menuPortfolio').on('click', function (e) {
+		e.preventDefault();
+		var targetElem = $('#portfolio');
+		portApp.scroll(targetElem);
+	});
+	$('#menuContact').on('click', function (e) {
+		e.preventDefault();
+		var targetElem = $('#contact');
+		portApp.scroll(targetElem);
+	});
+};
+
+// portApp.parallax = () => {
+// 	const controller = new ScrollMagic.Controller();
+// 	new ScrollMagic.Scene({
+// 		// duration: 1000, //this is how long scene lasts
+// 		offset: 0,
+// 		// triggerElement: '.greenArrow' //this is where the scene starts
+// 	})
+// 	.setClassToggle('.headerAndProfileWrap', 'backgroundColor')
+// 	.setPin('.text')
+// 	.addTo(controller);
+// }
+
+portApp.scroll = function (target) {
+	$('body, html').animate({
+		scrollTop: target.offset().top }, 'fast');
+};
+
 portApp.events = function () {
 	portApp.clickPortfolio();
 	portApp.hamburgerClick();
+	portApp.burgerMenuScroll();
 };
 
 portApp.init = function () {
 	portApp.events();
+	// portApp.parallax()
 };
 
 $(function () {
--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ console.log('hello')
 const portApp = {};
 
 
-portApp.scroll = () => {
-
-}
 
 portApp.hamburgerClick = () => {
 	$('#hamburger').on('click', () => {
@@ -35,14 +32,57 @@ portApp.clickPortfolio = () => {
 	})
 }
 
+portApp.burgerMenuScroll = () => {
+	$('#menuAbout').on('click', (e) => {
+		e.preventDefault();
+		let targetElem = $('#about');
+		portApp.scroll(targetElem);
+	});
+	$('#menuSkill').on('click', (e) => {
+		e.preventDefault();
+		let targetElem = $('#skills');
+		portApp.scroll(targetElem)
+	});
+	$('#menuPortfolio').on('click', (e) => {
+		e.preventDefault();
+		let targetElem = $('#portfolio')
+		portApp.scroll(targetElem)
+	})
+	$('#menuContact').on('click', (e) => {
+		e.preventDefault();
+		let targetElem = $('#contact')
+		portApp.scroll(targetElem)
+	})
+
+}
+
+// portApp.parallax = () => {
+// 	const controller = new ScrollMagic.Controller();
+// 	new ScrollMagic.Scene({
+// 		// duration: 1000, //this is how long scene lasts
+// 		offset: 0,
+// 		// triggerElement: '.greenArrow' //this is where the scene starts
+// 	})
+// 	.setClassToggle('.headerAndProfileWrap', 'backgroundColor')
+// 	.setPin('.text')
+// 	.addTo(controller);
+// }
+
+portApp.scroll = (target) => {
+	$('body, html').animate({
+		scrollTop: target.offset().top}, 'fast');
+}
+
 portApp.events = () => {
-	portApp.clickPortfolio()
-	portApp.hamburgerClick()
+	portApp.clickPortfolio();
+	portApp.hamburgerClick();
+	portApp.burgerMenuScroll();
 }
 
 
 portApp.init = () => {
 	portApp.events()
+	// portApp.parallax()
 }
 
 $(function() {
